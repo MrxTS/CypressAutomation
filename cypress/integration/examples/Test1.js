5d96adf694dd0d1ba2cec13668bfbcfd11f01c26
MrxTS <stefan.nguyen@me.com>
2022-11-01 20:47:54 +0100

    my first commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,12 @@
+//cypress - Spec = TestFile
+describe('My first Test Suite', function() {
+
+it('My first TestCase', function() {
+// test step
+
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+}   )
+
+
+}   )
